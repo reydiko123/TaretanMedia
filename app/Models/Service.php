be16241d1d888f316d7713f9759a757a8fa3a134
reydiko123 <reydikofakhran@gmail.com>
2026-09-09 19:11:45 +0700

    Perbaikan CI Checking

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use App\Support\SlugGenerator;
 use Database\Factories\ServiceFactory;
-use InvalidArgumentException;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use InvalidArgumentException;
 
 /**
  * @property int $id
